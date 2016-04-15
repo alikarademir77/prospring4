514fd79e0c98ef6239bd7d8e15d17c8381ef5642
Ali Karademir <akaradem@bestbuycanada.ca>
2016-04-14 21:36:23 -0700

    AspectJ Aspect usage with Spring

--- src/main/java/com/apress/prospring4/ch5/MessageWriter.java
+++ src/main/java/com/apress/prospring4/ch5/MessageWriter.java
@@ -2,6 +2,10 @@ package com.apress.prospring4.ch5;
 
 public class MessageWriter {
 	public void writeMessage() {
-		System.out.print("World");
+		System.out.println("foobar!");
+	}
+
+	public void foo() {
+		System.out.println("foo");
 	}
 }
